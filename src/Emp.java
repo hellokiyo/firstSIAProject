8f701463efe9f90cb0619406daeebfbd74178dc9
kimminachae1 <minchaeg10@gmail.com>
2025-07-18 16:40:33 +0900

    emp변경 메인변경

--- src/Emp.java
+++ src/Emp.java
@@ -6,10 +6,13 @@ public class Emp {
 	public String hire_date;
 	public String job_id;
 	public int salary;
+	public String lastName;
+	public String countryName;
+	
 	@Override
 	public String toString() {
-		return "id = " + id + ", email = " + email + ", phone = " + phone + ", hire_date = " + hire_date + ", job_id = "
-				+ job_id + ", salary = " + salary;
+		return "id=" + id + " email=" + email + ", phone=" + phone + ", hire_date=" + hire_date + ", job_id="
+				+ job_id + ", salary=" + salary + ", lastName=" + lastName + ", countryName=" + countryName;
 	}
 	
 	
--- src/EmpDAO.java
+++ src/EmpDAO.java
@@ -2,6 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,4 +33,33 @@ public static List<Emp> getDeptHeadFistName(String getFirstName) throws Exceptio
 		}
 		return result;
 	}
+
+public static List<Emp> getEmpListByCountryName(String cityName) throws Exception {
+	
+	// 나라 이름으로 그 나라에 근무하는 직원을 조회할 수 있는가
+	List<Emp> result = new ArrayList<Emp>();
+	Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/newhr","root","rootroot");
+	Statement stmt = conn.createStatement();
+	String sql = "select c.country_name, e.employee_id, e.last_name, e.phone_number, e.salary\r\n"
+					+ "from employees e\r\n"
+					+ "	join departments d on e.department_id = d.department_id\r\n"
+					+ "	join locations l on l.location_id = d.location_id\r\n"
+					+ "	join countries c on c.country_id = l.country_id " 
+					+ " where c.country_name = '" + cityName + "'";
+	ResultSet rs = stmt.executeQuery(sql);
+	while(rs.next()) {
+		Emp emp = new Emp();
+		emp.id = rs.getInt("employee_id");
+		emp.lastName = rs.getString("last_name");
+		emp.phone = rs.getString("phone_number");
+		emp.salary = rs.getInt("salary");
+		emp.countryName = rs.getString("country_name");
+		result.add(emp);
+	}
+	
+	return result;
+	
+}
+
+
 }
