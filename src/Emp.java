63b6a96feb814ea87584664323897bfec344c4d2
hellokiyo <ox2120@naver.com>
2025-07-18 13:13:50 +0900

    firstSIAProject

---
+++ src/Emp.java
@@ -0,0 +1,16 @@
+
+public class Emp {
+	public int id;
+	public String email;
+	public String phone;
+	public String hire_date;
+	public String job_id;
+	public int salary;
+	@Override
+	public String toString() {
+		return "id = " + id + ", email = " + email + ", phone = " + phone + ", hire_date = " + hire_date + ", job_id = "
+				+ job_id + ", salary = " + salary;
+	}
+	
+	
+}
