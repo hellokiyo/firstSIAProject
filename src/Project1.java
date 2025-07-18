808c7dbc765d7f9d39c3608e562d59844b1d9c4c
yunjungeee <bagyunjeong49@gmail.com>
2025-07-18 19:22:13 +0900

    프로젝트 수정

--- src/Project1.java
+++ src/Project1.java
@@ -2,7 +2,6 @@
 
 import java.util.List;
 import java.util.Scanner;
-import myjdbc4_1.EmpDAO;
 
 // 나라이름으로 그 나라에 근무하는 직원을 조회할 수 있는가?
 public class Project1 {
@@ -17,7 +16,7 @@ public class Project1 {
 		// 직원정보 전달받아서 그 나라의 직원이름 리턴
 		while(countryName != null && countryName.length() !=0 ) {
 			System.out.println("입력한 나라 이름 : " + countryName);
-			List<Emp> empList = EmpDAO1.getEmpListByCountryName(countryName);
+			List<Emp> empList = EmpDAO.getEmpListByCountryName(countryName);
 			System.out.printf("%1s\t %15s\t %13s\t %15s\t %10s\t \n", "country_name", "empolyee_id", "last_name", "phone_number", "salary");
 			for (Emp emp : empList) {
 				System.out.printf("%5s\t %15s\t %15s\t %20s\t %8s\t \n", emp.countryName, emp.id, emp.lastName, emp.phoneNumber, emp.salary);
