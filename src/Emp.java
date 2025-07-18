7ac40828354fce79fc6efc28c3a25bd63649ce16
hellokiyo <ox2120@naver.com>
2025-07-18 19:26:39 +0900

    수정

--- src/Emp.java
+++ src/Emp.java
@@ -11,12 +11,15 @@ public class Emp {
 	public int commission_pct;
 	public int department_id;
 	public int manager_id;
-	
-
 	@Override
 	public String toString() {
-
-		return id + "\t" + email + "\t" + phone + "\t" + hire_date + "\t" + job_id + "\t" + salary;
+		return "Emp [id=" + id + ", email=" + email + ", phone=" + phone + ", hire_date=" + hire_date + ", job_id="
+				+ job_id + ", salary=" + salary + ", lastName=" + lastName + ", countryName=" + countryName
+				+ ", commission_pct=" + commission_pct + ", department_id=" + department_id + ", manager_id="
+				+ manager_id + "]";
 	}
+	
+
+
 
 }
