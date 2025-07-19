146a927a7300fb44fb1c3fc48ad9a54e2574eb1e
hellokiyo <ox2120@naver.com>
2025-07-19 12:56:35 +0900

    수정

--- src/EmpDAO.java
+++ src/EmpDAO.java
@@ -5,18 +5,13 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-<<<<<<< HEAD
+
 import java.util.Scanner;
 
 public class EmpDAO {
+
 	public static List<Emp> getDeptHeadFistName(String getFirstName) throws Exception {
 
-=======
-
-public class EmpDAO {
-public static List<Emp> getDeptHeadFistName(String getFirstName) throws Exception {
-		
->>>>>>> f8c0fd721d6d9a12a0836b69d8353ee2fdca3c97
 		List<Emp> result = new ArrayList<Emp>();
 
 		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/newhr", "root", "rootroot");
@@ -42,7 +37,6 @@ public static List<Emp> getDeptHeadFistName(String getFirstName) throws Exceptio
 		return result;
 	}
 
-<<<<<<< HEAD
 	public static List<Emp> getEmpListByCountryName(String cityName) throws Exception {
 
 		System.out.println("나라 이름 : ");
@@ -223,67 +217,62 @@ public static List<Emp> getDeptHeadFistName(String getFirstName) throws Exceptio
 		}
 		return result;
 	}
-=======
-public static List<Emp> getEmpListByCountryName(String cityName) throws Exception {
-	
-	// 나라 이름으로 그 나라에 근무하는 직원을 조회할 수 있는가
-	List<Emp> result = new ArrayList<Emp>();
-	Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/newhr","root","rootroot");
-	Statement stmt = conn.createStatement();
-	String sql = "select c.country_name, e.employee_id, e.last_name, e.phone_number, e.salary\r\n"
-					+ "from employees e\r\n"
-					+ "	join departments d on e.department_id = d.department_id\r\n"
-					+ "	join locations l on l.location_id = d.location_id\r\n"
-					+ "	join countries c on c.country_id = l.country_id " 
-					+ " where c.country_name = '" + cityName + "'";
-	ResultSet rs = stmt.executeQuery(sql);
-	while(rs.next()) {
+
+	public static List<Emp> getEmpListByCountryName(String cityName) throws Exception {
+
+		// 나라 이름으로 그 나라에 근무하는 직원을 조회할 수 있는가
+		List<Emp> result = new ArrayList<Emp>();
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/newhr", "root", "rootroot");
+		Statement stmt = conn.createStatement();
+		String sql = "select c.country_name, e.employee_id, e.last_name, e.phone_number, e.salary\r\n"
+				+ "from employees e\r\n" + "	join departments d on e.department_id = d.department_id\r\n"
+				+ "	join locations l on l.location_id = d.location_id\r\n"
+				+ "	join countries c on c.country_id = l.country_id " + " where c.country_name = '" + cityName + "'";
+		ResultSet rs = stmt.executeQuery(sql);
+		while (rs.next()) {
+			Emp emp = new Emp();
+			emp.id = rs.getInt("employee_id");
+			emp.lastName = rs.getString("last_name");
+			emp.phone = rs.getString("phone_number");
+			emp.salary = rs.getInt("salary");
+			emp.countryName = rs.getString("country_name");
+			result.add(emp);
+		}
+
+		return result;
+
+	}
+
+	public static Emp getEmpByFirstNLastName(String firstName, String lastName) throws Exception {
+		// 직원 이름으로 직원정보를검색할 수 있는가?
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/newhr", "root", "rootroot");
+
+		String sql = "select * from employees where first_name = ? and last_name = ?";
+
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setString(1, firstName);
+		stmt.setString(2, lastName);
+
+		ResultSet rs = stmt.executeQuery();
 		Emp emp = new Emp();
-		emp.id = rs.getInt("employee_id");
-		emp.lastName = rs.getString("last_name");
-		emp.phone = rs.getString("phone_number");
-		emp.salary = rs.getInt("salary");
-		emp.countryName = rs.getString("country_name");
-		result.add(emp);
-	}
-	
-	return result;
-	
-<<<<<<< HEAD
-	
-	
-}
-public static Emp getEmpByFirstNLastName(String firstName, String lastName ) throws Exception {
-	//직원 이름으로 직원정보를검색할 수 있는가?
 
-	Connection conn = 
-			DriverManager.getConnection("jdbc:mysql://localhost:3307/newhr", "root", "rootroot");
-	
-	String sql = "select * from employees where first_name = ? and last_name = ?";
-	
-	PreparedStatement stmt = conn.prepareStatement(sql);
-	stmt.setString(1, firstName);
-	stmt.setString(2, lastName);
-	
-	ResultSet rs = stmt.executeQuery();
-	Emp emp = new Emp();
-	
-	while(rs.next())
-	{	
-		emp.id = rs.getInt("employee_id");
-		emp.email = rs.getString("email");
-		emp.phone = rs.getString("phone_number");
-		emp.hire_date = rs.getString("hire_date");
-		emp.job_id = rs.getString("job_id");
-		emp.salary = rs.getInt("salary");
-		emp.commission = rs.getInt("commission_pct");
-		emp.manager_id = rs.getInt("manager_id");
-		emp.department_id = rs.getInt("department_id");
+		while (rs.next()) {
+			emp.id = rs.getInt("employee_id");
+			emp.email = rs.getString("email");
+			emp.phone = rs.getString("phone_number");
+			emp.hire_date = rs.getString("hire_date");
+			emp.job_id = rs.getString("job_id");
+			emp.salary = rs.getInt("salary");
+			emp.commission = rs.getInt("commission_pct");
+			emp.manager_id = rs.getInt("manager_id");
+			emp.department_id = rs.getInt("department_id");
+
+		}
+		return emp;
 
 	}
-	return emp;
 
-}
 //입사년도로 검색할 수 있는가?
 public static List<Emp> getEmpListByYear(int year) throws Exception {
 	List<Emp> result = new ArrayList<Emp>();
@@ -306,11 +295,4 @@ public static List<Emp> getEmpListByYear(int year) throws Exception {
 		result.add(emp);
 	}
 	return result;
-
-=======
->>>>>>> e47ae052a8315287af373fdb610f3c83087368ef
-}
-
->>>>>>> f8c0fd721d6d9a12a0836b69d8353ee2fdca3c97
-
 }
