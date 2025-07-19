e47ae052a8315287af373fdb610f3c83087368ef
kimminachae1 <minchaeg10@gmail.com>
2025-07-19 11:59:44 +0900

    민채_ 오류수정 및 기능추가

--- src/Project1.java
+++ src/Project1.java
@@ -1,3 +1,5 @@
+
+
 import java.util.List;
 import java.util.Scanner;
 
@@ -33,7 +35,7 @@ public class Project1 {
 			} else if(selectNo == 6) {
 				
 			} else if(selectNo == 7) {
-				EmpDAO.getEmpListByCountryName();
+				EmpDAO.getEmpListByCountryName(null);
 			} else if(selectNo == "종료") {
 				System.out.println("프로그램 종료합니다");
 			} else {
@@ -57,12 +59,8 @@ public class Project1 {
 			System.out.println("입력한 나라 이름 : " + countryName);
 			List<Emp> empList = EmpDAO.getEmpListByCountryName(countryName);
 			System.out.printf("%1s\t %15s\t %13s\t %15s\t %10s\t \n", "country_name", "empolyee_id", "last_name", "phone_number", "salary");
-			System.out.printf("%5s\t %15s\t %15s\t %20s\t %8s\t \n", emp.countryName, emp.id, emp.lastName, emp.phoneNumber, emp.salary);
 			for (Emp emp : empList) {
-<<<<<<< HEAD
 				System.out.printf("%5s\t %15s\t %15s\t %20s\t %8s\t \n", emp.countryName, emp.id, emp.lastName, emp.phone, emp.salary);
-=======
->>>>>>> 7ed9bb8312166d172e263d3a95df752a307d97f5
 			}
 			System.out.println(countryName + "에서 근무하는 직원 수 : " + empList.size() + "명");
 			System.out.print("cityName(종료하려면 엔터를 누르세요.) :");
